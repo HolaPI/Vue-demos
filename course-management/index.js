199ce28182c955c00455175aaa24965e97b1bdfd
HolaPI <shevarwin@126.com>
2019-04-02 21:17:35 +0800

    connect MongoDB using mongoose

--- course-management/index.js
+++ course-management/index.js
@@ -1,11 +1,33 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/course-M', { useNewUrlParser: true })
+    .then(() => {
+        console.log('database is connected')
+    })
+    .catch(err => {
+        console.log(err)
+    })
+// mongoose.connection.once('open', () => {
+//     console.log('database is connected')
+// }).on('error', (err) => {
+//     console.log('connection failed: ', err)
+// })
+
+//import date-model
+require('./models/courseModel')
+const courseModel = mongoose.model('course')
 const app = express()
 //handlebars middleware
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }))
 app.set('view engine', 'handlebars')
+//bodyParser middlewaries
+let jsonParser = bodyParser.json();
+let urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 app.get('/', (req, res) => {
     // res.send('Index')
     let title = 'Ich bin Awenn, aus China'
@@ -16,8 +38,26 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about')
 });
-app.get('/idea', (req, res) => {
-    res.render('idea')
+app.post('/idea', urlencodedParser, (req, res) => {
+    // console.log(req.body)
+    let err = []
+    if (!req.body.title) {
+        err.push({ text: "input title plz" })
+    }
+    if (!req.body.details) {
+        err.push({ text: 'input details plz' })
+    }
+    if (err.length > 0) {
+        res.render('newCourse', {
+            errors: err,
+            title: req.body.title,
+            details: req.body.details
+        })
+    } else {
+        res.render('idea')
+    }
+
+
 });
 app.get('/new-course', (req, res) => {
     res.render('newCourse')
---
+++ course-management/models/courseModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+const courseModel = new Schema({
+    title: {
+        type: String,
+        require: true
+    },
+    details: {
+        type: String,
+        require: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+mongoose.model('course', courseModel)
+
