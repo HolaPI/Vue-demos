7f36997f7d4b927640ee160c8087e03022279b58
HolaPI <shevarwin@126.com>
2019-04-07 23:06:08 +0800

    completed login verification in course-M project

---
+++ course-management/models/userModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const userModel = new Schema({
+    userName: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+mongoose.model('user', userModel)
--- course-management/router/users.js
+++ course-management/router/users.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt')
 
 const router = express.Router()
 //import date-model
-require('../models/courseModel')
+require('../models/userModel')
 //instantiation, it can be used to achieve 'add, delete, update, find'
-const courseModel = mongoose.model('course')
+const userModel = mongoose.model('user')
+
 //bodyParser middlewaries
 let jsonParser = bodyParser.json()
 let urlencodedParser = bodyParser.urlencoded({ extended: false })
@@ -29,7 +31,28 @@ router.get('/user/login', (req, res) => {
     res.render('user/login')
 })
 router.post('/user/login', urlencodedParser, (req, res) => {
-    let errors = []
+    userModel.findOne({ email: req.body.email }).then((user) => {
+        if (!user) {
+            req.flash('error_msg', 'User Not Found')
+            res.redirect('/user/login')
+            return
+        } else {
+            bcrypt.compare(req.body.password, user.password, (err, isMatch) => {
+                if (err) throw err
+                if (!isMatch) {
+                    let err = [{ text: 'Password Not Paired!' }]
+                    res.render('user/login', {
+                        errors: err,
+                        email: req.body.email
+                    })
+                    return
+                } else {
+                    req.flash('success_msg', 'Login Successfully!')
+                    res.redirect('/idea')
+                }
+            })
+        }
+    })
 
 })
 router.get('/user/register', (req, res) => {
@@ -56,7 +79,35 @@ router.post('/user/register', urlencodedParser, (req, res) => {
             repassword: req.body.repassword
         })
     } else {
-        res.send('congrds')
+        userModel.findOne({ email: req.body.email })
+            .then((user) => {
+                if (user) {
+                    req.flash('error_msg', 'This email has been registed, change one.')
+                    res.redirect('/user/register')
+                } else {
+                    const newUser = new userModel({
+                        userName: req.body.name,
+                        email: req.body.email,
+                        password: req.body.password
+                    })
+                    bcrypt.genSalt(10, (err, salt) => {
+                        // if (err) throw err
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            if (err) throw err
+                            newUser.password = hash
+                            newUser.save().then(() => {
+                                req.flash('success_msg', 'Registed Successfully!')
+                                res.redirect('/user/login')
+                            }).catch(() => {
+                                req.flash('error_msg', 'Something Unexpected Happends.')
+                                res.redirect('/user/register')
+                            })
+                        })
+                    })
+
+                }
+            })
+
     }
 })
 module.exports = router
